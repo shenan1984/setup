2b31e4c3ef823542fd6b56156cce9d2eecf7de9c
shenan1984 <shenan1984@gmail.com>
2013-09-08 23:19:45 -0400

    deleted extra lines

--- mean_stack.sh
+++ mean_stack.sh
@@ -16,18 +16,6 @@ heroku config | grep "MONGOHQ"
 
 # Set up heroku configuration variables
 # https://devcenter.heroku.com/articles/config-vars
-# - Edit .env to include your own COINBASE_API_KEY and HEROKU_POSTGRES_URL.
 # - Modify the .env.dummy file, and DO NOT check .env into the git repository.
 # - See .env.dummy for details.
 cp $HOME/dotfiles/.env.dummy $HOME/mean/.env
-
-# Reminder to set up git
-# STRING=$( cat <<EOF
-# Now configure github using the following lines.\n\n
-# 1) git config --global user.name "Your Name Here"
-# 2) git config --global user.email "your_email@example.com"
-# 3) git remote rm origin
-# 4) git remote add origin git@github.com:Your_Username/New_Project_Name.git
-# 5) git add -f public/lib
-# EOF
-# )
