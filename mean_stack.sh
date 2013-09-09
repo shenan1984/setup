a70a51fed916dc41161ceea7ff5331b04125d438
shenan1984 <shenan1984@gmail.com>
2013-09-09 16:35:44 -0400

    Update mean_stack.sh

--- mean_stack.sh
+++ mean_stack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Clone MEAN stack
-# git pull https://github.com/linnovate/mean.git
+git pull https://github.com/linnovate/mean.git
 # sudo apt-get install mongodb-clients
 
 # Install packages and bower
@@ -9,14 +9,17 @@ npm install -g bower
 bower install
 
 # Set up heroku
-heroku login
 heroku create
 ssh-keygen -t rsa
 heroku keys:add
 heroku plugins:install git://github.com/ddollar/heroku-config.git
+heroku addons:add papertrail
+heroku addons:add mongohq:sandbox
+heroku config | grep "MONGOHQ"
 
 # Set up heroku configuration variables
 # https://devcenter.heroku.com/articles/config-vars
 # - Modify the .env.dummy file, and DO NOT check .env into the git repository.
 # - See .env.dummy for details.
-cp $HOME/dotfiles/.env.dummy $HOME/mean/.env
+cp $HOME/dotfiles/.env.dummy .env
+
