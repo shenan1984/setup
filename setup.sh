819cbbd06393a6e8bcb22ad553cb93f83065625e
shenan1984 <shenan1984@users.noreply.github.com>
2013-10-02 17:17:09 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,8 +14,8 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.18
-nvm use v0.10.18
+nvm install v0.10.20
+nvm use v0.10.20
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
